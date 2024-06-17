50d58323631aa4ad65de2b2836c6efd1ef77332f
priyanshu082 <priyanshusingh216@gmail.com>
2024-06-17 15:25:24 +0530

    5th lecture is done

--- app2.js
+++ app2.js
@@ -3,21 +3,41 @@ import ReactDOM from "react-dom/client";
 
 const heading = <h1>hellloooo</h1>;
 
- const Title=(a)=> {
-   return <h1>{a}</h1>
-}
-let a=43456
+const Title = (a) => {
+  return <h1>{a}</h1>;
+};
+let a = 43456;
 //name of component start with capital letter :- not mandatory
 const Component = () => {
   return (
     <div>
-       {Title(a)}
-{heading}
-        <h1>components</h1>
+      {Title(a)}
+      {heading}
+      <h1>components</h1>
     </div>
-)
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Component/>);
+root.render(<Component />);
+
+//React.Fragment || it will remove the extra div we are using to wrap the other components
+//jsx can only have one parent 
+
+const Component2 = () =>(
+      <React.Fragment>
+        {Title(a)}
+        {heading}
+        <h1>components</h1>
+      </React.Fragment>
+    );
+
+    // or we can use <> </>  this is same as React.Fragment
+
+
+    //virtual DOM
+    //react fibre structure
+    //no key<<<<<< index as key (ok ok) <<<< unique key(best practise)
+
+
